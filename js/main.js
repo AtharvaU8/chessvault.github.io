956a8d71410cdeb6d4b0a53a872bf2900b700307
Atharva Ugale <90385487+AtharvaU8@users.noreply.github.com>
2024-11-20 12:30:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -688,15 +688,6 @@ function downloadFile(data, filename) {
     }
 }
 
-try {
-    const response = await fetch("/path/to/resource");
-    if (!response.ok) {
-        throw new Error("Network response was not ok");
-    }
-} catch (error) {
-    console.error("Error fetching resource:", error);
-}
-
 function copyGameNotation() {
     // Get all moves in Standard Algebraic Notation (SAN)
     const moves = game.history({ verbose: true });
