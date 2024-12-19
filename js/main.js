ed9da62f95bca919b22e60f34520df097b040f35
Atharva Ugale <90385487+AtharvaU8@users.noreply.github.com>
2024-12-19 22:33:25 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -935,3 +935,9 @@ document.querySelector("#fileInput").onchange = function() {
         }
     }
 };
+
+const menu = document.querySelector(".menu-btn i").addEventListener("click", () => {
+  document.querySelector(".navbar .menu").classList.toggle("active");
+  document.querySelector(".menu-btn i").classList.toggle("active");
+  document.querySelector(".navbar .max-width .menu .inaclogo").classList.toggle("aclogo");
+});
